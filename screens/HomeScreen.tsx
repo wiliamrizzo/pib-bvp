6aa4657a324c9c1f5012ede9fa32e48bbdb93a89
wiliamrizzogit config --global user.namegit config --global user.name wiliamrizzogit config --global user.name wiliamrizzo <wiliamrizzo@gmail.com>
2020-10-16 00:13:39 -0300

    logo redondo

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,47 +1,43 @@
 import * as React from "react";
 import { StyleSheet, Image } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 
 export default function HomeScreen() {
     return (
-        <View style={styles.container}>
-            <View>
-                <Image
-                    style={styles.image}
-                    source={require("../assets/images/logo-pib.jpg")}
-                />
-                {/* <Text style={styles.title}>Primeira Igreja Batista em Bela Vista do Paraíso</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.js" /> */}
-            </View>
+        <View
+            style={styles.container}
+            lightColor="rgba(255,255,255,0.1)"
+            darkColor="#eeeccc"
+        >
+            {/* <ScrollView> */}
+            <Image
+                style={styles.image}
+                source={require("../assets/images/logo-pib.jpg")}
+            />
+            {/* </ScrollView> */}
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    image: {
-        //flex: 1,
-        //width: "100%",
-        borderWidth: 11,
-        borderRadius: 400,
-        height: 400,
-        resizeMode: "contain",
-        //backgroundColor: '#fff'
-    },
     container: {
         flex: 1,
         alignItems: "center",
         justifyContent: "center",
     },
+    image: {
+        borderWidth: 1,
+        borderRadius: 150,
+        height: 300,
+        width: 300,
+        resizeMode: "contain",
+        backgroundColor: "transparent",
+    },
     title: {
         fontSize: 20,
         fontWeight: "bold",
-    },
-    separator: {
-        marginVertical: 30,
-        height: 2,
-        width: "80%",
     },
 });
